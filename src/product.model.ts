8ab8db4620158794a321f5b4ed0aad71159980d2
Bryan Windsor <bryan.windsor@ordina.nl>
2020-02-15 00:15:28 +0100

    added class-validator library

---
+++ src/product.model.ts
@@ -0,0 +1,18 @@
+import { IsNumber, IsNotEmpty, IsPositive } from "class-validator";
+
+export class Product {
+  @IsNotEmpty()
+  title: string;
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return;
+  }
+}
